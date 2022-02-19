11fb5318d9aa8694b768ac6fbad98186cca7e638
nakanoi <nakanoinryota@gmail.com>
2022-02-19 21:51:08 +0900

    Add simulated server class.

---
+++ src/main/java/com/nakanoi/juniter/sec9/Server.java
@@ -0,0 +1,12 @@
+package com.nakanoi.juniter.sec9;
+
+/** Simulated server class. */
+public class Server {
+  public void start() {}
+
+  public String get(String path) {
+    return path;
+  }
+
+  public void shutdown() {}
+}
