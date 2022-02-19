4cc2ddd872895a2cb2209e9b95e07e602ef865dc
nakanoi <nakanoinryota@gmail.com>
2022-02-19 22:17:39 +0900

    Add server status.

--- src/main/java/com/nakanoi/juniter/sec9/Server.java
+++ src/main/java/com/nakanoi/juniter/sec9/Server.java
@@ -2,11 +2,21 @@ package com.nakanoi.juniter.sec9;
 
 /** Simulated server class. */
 public class Server {
-  public void start() {}
+  private boolean isOn = false;
+
+  public void start() {
+    isOn = true;
+  }
 
   public String get(String path) {
     return path;
   }
 
-  public void shutdown() {}
+  public void shutdown() {
+    isOn = false;
+  }
+
+  public boolean getStatus() {
+    return isOn;
+  }
 }
