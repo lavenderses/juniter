1c2ba681cd095d67ab2f7a7dc4d072efa9ee78ef
nakanoi <nakanoinryota@gmail.com>
2022-02-19 22:58:51 +0900

    Add verifier and its tests.

--- src/test/java/com/nakanoi/juniter/sec9/ExternalResourceExampleTest.java
+++ src/test/java/com/nakanoi/juniter/sec9/ExternalResourceExampleTest.java
@@ -8,7 +8,8 @@ import org.junit.jupiter.api.extension.RegisterExtension;
 /** Test Server Resource implemented ExternalResource. */
 public class ExternalResourceExampleTest {
   @RegisterExtension
-  static ExternalResourceExampleExtension serverResource = ExternalResourceExampleExtension.builder().build();
+  static ExternalResourceExampleExtension serverResource =
+      ExternalResourceExampleExtension.builder().build();
 
   /** Test server returns string which is equal to the request path. */
   @Test
