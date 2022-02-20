6d665ba51a0a11d2c67ef748773b1254c7449493
nakanoi <nakanoinryota@gmail.com>
2022-02-20 16:17:04 +0900

    Add Categories with @Tag.

---
+++ src/test/java/com/nakanoi/juniter/sec10/FooTest.java
@@ -0,0 +1,24 @@
+package com.nakanoi.juniter.sec10;
+
+import static org.assertj.core.api.Assertions.assertThat;
+
+import org.junit.jupiter.api.Tag;
+import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.TestInfo;
+
+/** Test mixed in slow and fast tests. */
+public class FooTest {
+  @Test
+  @Tag("slow-test")
+  void slowTest(TestInfo testInfo) {
+    assertThat(testInfo.getTags().contains("slow-test")).isTrue();
+    assertThat(testInfo.getTags().contains("fast-test")).isFalse();
+  }
+
+  @Test
+  @Tag("fast-test")
+  void fastTest(TestInfo testInfo) {
+    assertThat(testInfo.getTags().contains("slow-test")).isFalse();
+    assertThat(testInfo.getTags().contains("fast-test")).isTrue();
+  }
+}
